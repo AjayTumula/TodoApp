401c9b8b750f7ace0939916694832550ea79b460
AjayTumula <ajaytumula72@gmail.com>
2024-08-13 16:26:57 +0530

    Update validations

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express();
 const port = 3000;
+
 
 app.use(express.json());
 
 app.post('/todo', (req, res) => {
-    
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload) {
+        res.send(411).json({
+            msg: "You sent wrong inputs"
+        })
+        return;
+    }
+    //put it in mongodb
 })
 
 app.get('/todos', (req, res) => {
@@ -13,7 +23,15 @@ app.get('/todos', (req, res) => {
 })
 
 app.put('/completed', (req, res) => {
-    
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload) {
+        res.send(411).jaon({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+    //put in mongo db
 })
 
 app.listen(port, () => {
