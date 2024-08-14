6c29c755fbae00a5a6f67b39db016309597bcd8e
AjayTumula <ajaytumula72@gmail.com>
2024-08-14 12:55:14 +0530

    add the frontend-backend logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,18 @@ import { Todos } from './components/Todos'
 
 function App() {
 
+  const [ todos, setTodos ] = useState([]);
+
+  fetch("http://localhost:3001/todos")
+  .then(async function(res) {
+    const json = await res.json();
+    setTodos(json.todos);
+  })
+
   return (
     <div>
       <CreateTodo></CreateTodo>
-      <Todos></Todos>
+      <Todos todos={todos}></Todos>
     </div>
   )
 }
