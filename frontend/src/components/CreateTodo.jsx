1b7d49a851a5555119b20eb019382b6b0e5e1d5f
AjayTumula <ajaytumula72@gmail.com>
2024-08-14 11:29:11 +0530

    add frontend for todo

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,9 @@
+
+export function CreateTodo() {
+    return <div>
+        <input style={{margin: 10, padding: 10 }} type="text" placeholder="title"></input> <br />
+        <input style={{margin: 10, padding: 10 }} type="text" placeholder="description"></input> <br />
+
+        <button style={{margin: 10, padding: 10 }}>Add a todo</button>
+    </div>
+}
